cdaf87f4e4b53f7f490efd29b8af2dc61753615d
liux0229 <liux0229@gmail.com>
2013-10-19 23:54:28 -0700

    Finish the rest of the work.

--- cpp/vector/Vector.h
+++ cpp/vector/Vector.h
@@ -1,8 +1,20 @@
 #include <memory>
 #include <utility>
 #include <iostream>
+#include <type_traits>
+#include <iterator>
 
 namespace library {
+
+template<typename In, typename Out>
+Out uninitialized_move(In s, In e, Out out) {
+  using T = typename std::iterator_traits<In>::value_type;
+  for (; s != e; ++s, ++out) {
+    new (static_cast<void *>(&*out)) T{ std::move(*s) };
+    s->~T();
+  }
+  return out;
+}
 
 template<typename T, typename A = std::allocator<T>>
 struct VectorBase;
@@ -70,8 +82,13 @@ class Vector {
   iterator end() const { return base_.space; }
   const_iterator cbegin() const { return base_.elem; }
   const_iterator cend() const { return base_.space; }
+
   SizeType size() const { return base_.space - base_.elem; }
   SizeType capacity() const { return base_.last - base_.elem; }
+
+  const T& operator[](size_t x) const {
+    return *(begin() + x);
+  }
 
   explicit Vector(SizeType n = 0, const T& v = T{}, A alloc = A{})
     : base_(alloc, n) {
@@ -97,6 +114,39 @@ class Vector {
     swap(*this, t);
     return *this;
   }
+
+  void reserve(SizeType newSize) {
+    if (newSize > capacity()) {
+      VectorBase<T, A> newBase{base_.alloc, size(), newSize - size()};
+      uninitialized_move(base_.elem, base_.space, newBase.elem);
+      swap(base_, newBase);
+    }
+  }
+
+  void resize(SizeType newSize, const T& val = T{}) {
+    if (newSize > capacity()) {
+      reserve(newSize);
+    }
+    if (newSize >= size()) {
+      for (auto p = base_.space; p != base_.elem + newSize; ++p) {
+        base_.alloc.construct(p, val);
+      }
+    } else {
+      for (auto p = base_.elem + newSize; p != base_.space; ++p) {
+        base_.alloc.destroy(p);
+      }
+    }
+    base_.space = base_.elem + newSize;
+  }
+
+  void push_back(const T& val) {
+    if (size() >= capacity()) {
+      reserve(capacity() == 0 ? 8 : capacity() * 2);
+    }
+    base_.alloc.construct(base_.space, val);
+    ++base_.space;
+  }
+
  private:
   friend void swap(Vector& a, Vector& b) {
     swap(a.base_, b.base_);
--- cpp/vector/VectorTest.cpp
+++ cpp/vector/VectorTest.cpp
@@ -5,48 +5,116 @@
 using namespace std;
 using namespace library;
 
-void check(bool c) {
-  assert(c);
-}
+#define CHECK_EQ(a, b) \
+  do { \
+    if ((a) != (b)) { \
+      cerr << "CHECK_EQ: " << #a << "<" << (a) << "> != " \
+           << #b << "<" << (b) << ">" << endl; \
+      assert(false); \
+    } \
+  } while (false)
 
-void check(const Vector<int>& v, int val, size_t size, size_t capacity) {
-  check(v.size() == size);
-  check(v.capacity() == capacity);
-  for (int x : v) {
-    check(x == val);
+template<typename T>
+void check(const Vector<T>& v, const T& val, size_t size, size_t capacity) {
+  CHECK_EQ(size, v.size());
+  CHECK_EQ(capacity, v.capacity());
+  for (const T& x : v) {
+    CHECK_EQ(val, x);
   }
 }
-void check(const Vector<int>& v, int val, size_t size) {
+
+template<typename T>
+void check(const Vector<T>& v, const T& val, size_t size) {
   check(v, val, size, size);
 }
 
+struct Element {
+  Element() { }
+  Element(int xx) : x(xx) { }
+  int x { 0 };
+};
+
+bool operator==(const Element& a, const Element& b) { 
+  return a.x == b.x; 
+}
+bool operator!=(const Element& a, const Element& b) { 
+  return !(a == b);
+}
+
+ostream& operator<<(ostream& out, const Element& e) {
+  out << e.x;
+  return out;
+}
+
 void TestVector() {
+  using VE = Vector<Element>;
   {
-    Vector<int> v;
-    check(v, 0, 0);
+    VE v;
+    check(v, Element{0}, 0);
   }
   {
-    Vector<int> v(10);
-    check(v, 0, 10);
+    VE v(10);
+    check(v, Element{0}, 10);
   }
   {
-    int val = 1001;
-    Vector<int> v(10, val);
-    check(v, val, 10);
+    Element val1 {1001};
+    int n1 = 10;
+    VE v1(n1, val1);
+    check(v1, val1, n1);
 
-    Vector<int> v2{v};
-    check(v2, val, v.size());
+    VE v2{v1};
+    check(v2, val1, v1.size());
 
-    Vector<int> v3(20, 501);
+    Element val2 {501};
+    int n2 = 20;
+    VE v3(n2, val2);
     v2 = v3;
     check(v2, *v3.begin(), v3.size());
 
-    Vector<int> v4{move(v3)};
-    check(v4, 501, 20);
+    VE v4{move(v3)};
+    check(v4, val2, n2);
+
+    v4 = move(v1);
+    check(v4, val1, n1);
+  }
+  {
+    VE v(10, Element{1});
+    v.resize(20, Element{2});
+    CHECK_EQ(20, v.size());
+    CHECK_EQ(20, v.capacity());
+    for (size_t i = 0; i < 10; ++i) {
+      CHECK_EQ(Element{1}, v[i]);
+    }
+    for (size_t i = 10; i < 20; ++i) {
+      CHECK_EQ(Element{2}, v[i]);
+    }
+    v.resize(15);
+    CHECK_EQ(15, v.size());
+    CHECK_EQ(20, v.capacity());
+    for (size_t i = 0; i < 10; ++i) {
+      CHECK_EQ(Element{1}, v[i]);
+    }
+    for (size_t i = 10; i < 15; ++i) {
+      CHECK_EQ(Element{2}, v[i]);
+    }
+  }
+  {
+    VE v(10, Element{1});
+    v.reserve(20);
+    check(v, Element{1}, 10, 20);
+  }
+  {
+    VE v;
+    for (int i = 0; i < 1000; ++i) {
+      v.push_back(Element{1001});
+    }
+    check(v, Element{1001}, 1000, 1024);
   }
 }
 
 int main() {
+  Element e;
+  Element e2{move(e)};
   TestVector();
   cout << "All tests passed" << endl;
 }
