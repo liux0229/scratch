c13c2e3e35f5c993f4c0e609b249d036c0a157eb
Rocky Liu <liux0229@gmail.com>
2014-04-20 12:22:10 -0700

    Start Paxos project

---
+++ Paxos/source/Legislator.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Legislator {
+public:
+  Legislator(size_t chamberSize) {
+
+  }
+};
--- algorithm/codejam/2013/FaireSquareHard2.cpp
+++ algorithm/codejam/2013/FaireSquareHard2.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <iostream>
 #include <cstring>
 #include <cmath>
 #include <string>
@@ -8,119 +8,93 @@ namespace FairSquareHard2 {
 
 using namespace std;
 
-const int N = 105;
-const int MAX_HALF_LEN = 50;
+typedef long long int64_t;
 
-string square(const string& x)
-{
-	static char buf[N];
-	memset(buf, 0, sizeof buf);
-	int len = x.size();
-	for (int i = 0; i < len; i++) {
-		for (int j = 0; j < len; j++) {
-			int p = x[i] * x[j];
-			buf[i + j] += p;
-			buf[i + j + 1] += buf[i + j] / 10;
-			buf[i + j] %= 10;
-		}
+class BigNum {
+public:
+	BigNum() {
+		d_.resize(1, 0);
 	}
-	int retLen = buf[len * 2 - 1] == 0 ? len * 2 - 1 : len * 2;
-	string ret(retLen, static_cast<char>(0));
-	for (int i = 0; i < retLen; i++) ret[i] = buf[i];
-	return ret;
-}
 
-bool LE(const string& a, const string& b)
-{
-	int la = a.size(), lb = b.size();
-	if (la < lb) return true;
-	else if (la > lb) return false;
-	for (int i = la - 1; i >= 0; i--) {
-		if (a[i] < b[i]) return true;
-		else if (a[i] > b[i]) return false;
+	BigNum sqrt() const {
+		// implement this
+		return *this;
 	}
-	return true;
-}
 
-void Print(const string& x)
-{
-	string v(x);
-	for (int i = 0; i < v.size(); ++i) v[i] += '0';
-	printf("%s\n", v.c_str());
-}
+	const vector<short>& digits() const { return d_; }
 
-bool isMatch(const string &x, const string&a, const string &b)
-{
-	int len = x.size();
-	if (x[len - 1] == 0) return false;
+	size_t size() const { return d_.size(); }
 
-	// Print(x);
-
-	string x2 = square(x);
-	return LE(a, x2) && LE(x2, b);
-}
-
-bool twoExpandable(const string& x)
-{
-	int oneCount = 0;
-	for (int i = 0; i < x.size(); ++i) {
-		if (x[i] == 0) continue;
-		else if (x[i] == 1) {
-			if (++oneCount > 1) {
-				return false;
+private:
+	// assume inputs are all digits
+	void read(istream& in) {
+		string s;
+		in >> s;
+		int start = 0;
+		for (; start < static_cast<int>(s.size()) - 1; start++) {
+			if (s[start] != '0') {
+				break;
 			}
-		} else {
-			return false;
+		}
+		vector<short> d;
+		d.reserve(s.size() - start);
+		for (int i = s.size() - 1; i >= start; i--) {
+			d.push_back(s[i] - '0');
+		}
+
+		d_.swap(d);
+	}
+
+	friend istream& operator>>(istream& in, BigNum& n) { 
+		n.read(in);
+		return in;
+	}
+
+	void write(ostream& out) const {
+		for (int i = d_.size() - 1; i >= 0; i--) {
+			out << d_[i];
 		}
 	}
-	return true;
+
+	friend ostream& operator<<(ostream& out, const BigNum& n) {
+		n.write(out);
+		return out;
+	}
+
+	vector<short> d_; 
+};
+
+// find all n <= x such that n is a parlindrome and n^2 is a parlindrome
+int64_t f(const BigNum& x) {
+	auto& ds = x.digits();
+	int len = x.size();
+	for (int i = len - 1; i >= 0; i--) {
+		// digits chosen left to this position match those of x
+		for (int d = 0; d < ds[i]; d++) {
+			
+		}
+	}
 }
 
-long long solve(const string& a, const string& b)
+int64_t solve(const BigNum& a, const BigNum &b)
 {
-	const char c0 = static_cast<char>(0);
-	const char c1 = static_cast<char>(1);
-	const char c2 = static_cast<char>(2);
-	const char c3 = static_cast<char>(3);
-	queue<string> q;
-	q.push("");
-	q.push(string(1, c0));
-	q.push(string(1, c1));
-	q.push(string(1, c2));
-	q.push(string(1, c3));
-	long long ret = 0;
-	while (!q.empty()) {
-		string x = q.front();
-		q.pop();
-#if 0
-		if (isMatch(x, a, b)) {
-			++ret;
-		}
-#endif
-		++ret;
-		if (x.size() >= MAX_HALF_LEN - 1) continue;
-		char last = x[x.size() - 1];
-		if ((last == c2 && x.size() > 1) || last == c3) continue;
-		q.push(c0 + x + c0);
-		q.push(c1 + x + c1);
-		if (twoExpandable(x)) {
-			q.push(c2 + x + c2);
-		}
-	}
-	printf("finishes\n");
-	return ret;
+	// implement square root
+	BigNum ra = a.sqrt();
+	BigNum rb = b.sqrt();
+	
+	// if ra^2 < a, we want to count [ra + 1, rb], and that translates to f(rb) - f(ra)
+	return f(rb) - f(ra);
 }
 
 int main()
 {
 	int T;
-	scanf("%d", &T);
-	for (int t = 0; t < T; t++) {
-		printf("Case #%d: ", t + 1);
-		char a[N], b[N];
-		scanf("%s %s", a, b);
-		long long ret = solve(a, b);
-		printf("%lld\n", ret);
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cout << "Case #" << (i + 1) << ": ";
+		BigNum a, b;
+		cin >> a >> b;
+		cout << solve(a, b);
 	}
 	return 0;
 }
