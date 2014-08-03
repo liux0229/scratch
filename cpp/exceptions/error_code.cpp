e00c6c32af3644f58edd962f4b9abb82b70f4017
Rocky Liu <liux0229@gmail.com>
2014-08-03 13:11:09 -0700

    Create error_code.cpp

---
+++ cpp/exceptions/error_code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <system_error>
+#include <future>
+
+using namespace std;
+
+namespace std {
+
+template<>
+struct is_error_code_enum<errc> : true_type {
+};
+
+}
+
+int main()
+{
+   error_code code(EPIPE, system_category()); 
+   cout << boolalpha << (EPIPE == static_cast<int>(errc::broken_pipe)) << endl;
+   cout << code.message() << endl;
+   
+   cout << error_code{EINTR, system_category()}.message() << endl;
+   cout << std::is_error_code_enum<errc>::value << endl;
+   
+   // how does the implementation know how to map this to the error message?
+   cout << error_code{errc::resource_unavailable_try_again}.message() << endl;
+   // outputs generic
+   cout << error_code{errc::resource_unavailable_try_again}.category().name() << endl;
+   
+   cout << error_code{future_errc::no_state}.message() << endl;
+   cout << error_code{future_errc::no_state}.category().name() << endl;
+   
+   return 0;
+}
