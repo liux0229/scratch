ed817696f0f6aea3b69d694c80f42eb30d656393
Rocky Liu <liux0229@gmail.com>
2014-09-08 00:08:50 -0700

    Delete dtrace-collection.d

--- tools/dtrace-collection.d
+++
@@ -1,7 +0,0 @@
-sudo dtrace -n 'proc:::exec-success { trace(func(pc)); }'
-
-sudo dtrace -n 'syscall::read:return { @["size"] = quantize(arg0); }'
-
-sudo dtrace -n 'syscall::open:entry { printf("%s %s", execname, copyinstr(arg0); }'
-
-sudo dtrace -n 'profile:::profile-99 { @[stack()] = count(); }'
