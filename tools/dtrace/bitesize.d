ebd6d5c6d4d8a0bdbde0d06eea6977a6f89020ba
Rocky Liu <liux0229@gmail.com>
2014-09-08 00:16:27 -0700

    Update bitesize.d

--- tools/dtrace/bitesize.d
+++ tools/dtrace/bitesize.d
@@ -8,11 +8,11 @@ dtrace:::BEGIN
 
 io:::start
 {
-  this->size = args[0]->b_count;
-  @Size[pid, curpsinfo->pr_psargs] = quantize(this->size);
+    this->size = args[0]->b_bcount;
+    @Size[pid, curpsinfo->pr_psargs] = quantize(this->size);
 }
 
 dtrace:::END
 {
-  printa("%8d %S\n%@d\n", @Size);
+    printa("%d %s\n%@d\n", @Size);
 }
