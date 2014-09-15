58c1de3794c3e18f36ba2d7472911d8080b726e1
Rocky Liu <liux0229@gmail.com>
2014-09-15 00:08:24 -0700

    Update one-liners.d

--- tools/dtrace/one-liners.d
+++ tools/dtrace/one-liners.d
@@ -5,3 +5,5 @@ sudo dtrace -n 'syscall::read:return { @["size"] = quantize(arg0); }'
 sudo dtrace -n 'syscall::open:entry { printf("%s %s", execname, copyinstr(arg0); }'
 
 sudo dtrace -n 'profile:::profile-99 { @[stack()] = count(); }'
+
+sudo dtrace -qn 'syscall::kill:entry { printf("%Y: %s (pid: %d) sends signal %d to pid %d\n", walltimestamp, execname, pid, arg1, arg0); }'
