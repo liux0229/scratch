bf598ef396c8dbe2233bf330c0d2d3b18191db1d
liux0229@gmail.com <rockyliu@fb.com>
2018-06-13 20:50:26 -0700

    Update sync.sh
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- sync.sh
+++ sync.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
+git pull
+cp mnist/readme.md /tmp/
 rm -fr mnist/
 cp -r /data/users/rockyliu/fbsource/fbcode/experimental/rockyliu/mnist/ .
 cp /data/users/rockyliu/notebooks/MNIST.ipynb mnist/
+cp /tmp/readme.md mnist/
 git add .
 git commit
-git pull
-ls mnist/readme.md
 git push
